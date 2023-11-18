429ce0d1893239aba6eacd626264d71b8c5c1661
xiaoliu <3133976825@qq.com>
2023-11-18 16:42:29 +0800

    first commit

--- src/views/music_house/index.vue
+++ src/views/music_house/index.vue
@@ -8,6 +8,7 @@
      </div>
     </div>
    </template>
+   
    <script  setup>
    import * as echarts from 'echarts'
    import chinaJSON from '@/assets/json/map.json'
@@ -30,19 +31,18 @@
       areaColor: '#fe9b45',
       opacity: 1,
      },
+     
     },
     {
      name: '陕西省',
      itemStyle: {
       areaColor: '#fd691b',
       opacity: 1,
-      // 陕西省特有的民歌信息
-      /*
-    song: '信天游：兰花花',
-    // 陕西省特有的音乐链接
-    audio: require('@/assets/music/lanhua.mp4'), 
-    */
      },
+      // 陕西省特有的民歌信息
+      song: '信天游：兰花花花花花花',
+     // 陕西省特有的音乐链接
+     //audio: require('@/assets/music/lanhua.mp4'), 
     },
     {
      name: '黑龙江省',
@@ -57,13 +57,13 @@
     { name: '兰州', value: [103.86615, 36.040129, 9] },
     { name: '新疆', value: [87.613228, 43.810394, 9] },
     { name: '长沙', value: [112.915204, 28.207735, 9] },
-    { name: '西安', value: [108.953445, 34.288842, 9] },
+    { name: '陕西省', value: [108.953445, 34.288842, 9] },
     { name: '云南', value: [102.710002, 25.045806, 9] },
    ]
    function drawChina() {
-    var myChart = echarts.init(chinaMap.value)
+    let myChart = echarts.init(chinaMap.value)
     echarts.registerMap('china', chinaJSON) //注册可用的地图
-    var option = {
+    let option = {
      geo: {
       map: 'china',
       roam: true, //是否允许缩放，拖拽
@@ -104,11 +104,11 @@
        hoverAnimation: true, //是否开启鼠标 hover 的提示动画效果
        label: {
         //图形上的文本标签，可用于说明图形的一些数据信息，比如值，名称等，
-        normal: {
+        
          formatter: '{b}',
          position: 'right',
          show: true,
-        },
+        
        },
        itemStyle: {
        //图形样式，normal 是图形在默认状态下的样式；emphasis 是图形在高亮状态下的样式，比如在鼠标悬浮或者图例联动高亮时
@@ -120,35 +120,40 @@
         },
        },
        zlevel: 1,
-       /*
        tooltip: {
-          show: true,
+          show: true,        
           formatter: function (params) {
             // 根据省份获取对应的民歌信息
-            const regionData = regions.find(region => region.name === params.name)
-            return `民歌: ${regionData.song}`
+           // console.log('Province Name:', params.name);
+            const regionData = regions.find(region => region.name === params.name);
+          //  console.log('Region Data:', regionData);
+             return `民歌: ${regionData ? regionData.song : '未知'}`;
           },
-        }*/
+        }
       },
     }
     myChart.setOption(option)
-    /*
-    mychart.on('mouseover', 'series', function (params) {
+    
+  myChart.on('mouseover', 'series', function (params) {
+      console.log('Mouse over:', params);
     const regionData = regions.find(region => region.name === params.name)
-    if (regionData.name === '陕西省') {
+    console.log('2.regionData',regionData)
+    if (regionData && regionData.name === '陕西省') {
       // 显示文本框
+      console.log('3.regionData',regionData.song)
       showTextBox(regionData)
     }
   })
 
+
   // 添加鼠标离开事件，隐藏文本框
-  mychart.on('mouseout', 'series', function () {
+  myChart.on('mouseout', 'series', function () {
     // 隐藏文本框
     hideTextBox()
   })
 
   // 添加点击事件，点击播放对应省份的音乐
-  mychart.on('click', function (params) {
+  myChart.on('click', function (params) {
     const regionData = regions.find(region => region.name === params.name)
     // 在这里可以处理播放音乐的逻辑，使用 regionData.audio 播放音乐
     // 例如，可以使用 HTML5 Audio API 播放音乐
@@ -157,29 +162,43 @@
   })
 }
 
-// 显示文本框的函数
 function showTextBox(regionData) {
-  const textBox = document.createElement('div')
-  textBox.className = 'text-box'
-  textBox.innerHTML = regionData.song
-  document.body.appendChild(textBox)
+  console.log('Show text box:', regionData.name);
+  const textBox = document.createElement('div');
+  textBox.className = 'text-box';
+  textBox.innerHTML = regionData.song;
+  textBox.style.position = 'absolute';
+  textBox.style.top = '550px'; // 调整为适当的位置
+  textBox.style.left = '50%'; // 调整为适当的位置
+  textBox.style.transform = 'translateX(-50%)'; // 居中显示
+  textBox.style.zIndex = '99999999'; // 设置较高的层级
+
+    document.body.appendChild(textBox);
+
+  document.body.appendChild(textBox);
 
   // 添加点击事件，点击播放音乐
   textBox.addEventListener('click', function () {
     // 在这里可以处理点击播放音乐的逻辑，使用 regionData.audio 播放音乐
     // 例如，可以使用 HTML5 Audio API 播放音乐
-    const audio = new Audio(regionData.audio)
-    audio.play()
-  })
+    const audio = new Audio(regionData.audio);
+    audio.play();
+  });
 }
+
 
 // 隐藏文本框的函数
 function hideTextBox() {
+  console.log('Hide text box');
   const textBox = document.querySelector('.text-box')
   if (textBox) {
-    textBox.parentNode.removeChild(textBox)
+    //textBox.parentNode.removeChild(textBox)
+    document.body.removeChild(textBox);
   }
 }
-*/
-}
    </script>
+   <style>
+   .text-box {
+  background-color: #89fa83; /* 设置背景颜色 */
+    }
+  </style>
